8ce1b35d6b654ae2af616d4b56f8c82937a0a1a2
Leland Sequel <leland@candlstrategy.com>
2025-12-24 20:05:11 +0000

    Fix possible blank screen issues and update build

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Debug from "./pages/Debug";
 import AustinNeighborhood from "./pages/locations/AustinNeighborhood";
 import { ThemeProvider } from "@/components/theme-provider";
 
@@ -10,11 +11,13 @@ export default function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/debug" element={<Debug />} />
           <Route path="/locations/:neighborhood" element={<AustinNeighborhood />} />
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
   );
 }
+
 
 
